d30c7ddcd93c95b30cde8c16464c87de1470f73c
wheelDestiny <672845190@qq.com>
2020-09-03 14:40:34 +0800

    Initial commit

---
+++ src/main/java/com/wheelDestiny/etl/ETLMapper.java
@@ -0,0 +1,54 @@
+package com.wheelDestiny.etl;
+
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class ETLMapper extends Mapper<LongWritable, Text,Text, NullWritable> {
+    private Text k = new Text();
+
+    private StringBuilder stringBuilder=new StringBuilder();
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+
+        String result = handleLine(line);
+        if(result == null){
+            context.getCounter("ETL","False").increment(1);
+        }else {
+            context.getCounter("ETL","True").increment(1);
+            k.set(result);
+            context.write(k,NullWritable.get());
+        }
+    }
+
+    /**
+     * ETL方法，处理掉长度不够的数据，并且把数据形式做转换
+     * @param l 输入的行
+     * @return  处理后的行
+     */
+    private String handleLine(String l) {
+        String[] fields = l.split("\t");
+        if (fields.length<9){
+            return null;
+        }
+        stringBuilder.delete(0,stringBuilder.length());
+        fields[3] = fields[3].replace(" ", "");
+
+        for (int i = 0; i < fields.length; i++) {
+            if(i==fields.length-1){
+                stringBuilder.append(fields[i]);
+            }else if(i<9){
+                stringBuilder.append(fields[i]).append("\t");
+            }else {
+                stringBuilder.append(fields[i]).append("&");
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+}
